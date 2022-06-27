9f2287dd4c204fdf7d5e6cb064c265c672330b02
MCKevmeister <markchristison@gmail.com>
2022-06-28 11:35:50 +1200

    add listing component

---
+++ resources/js/Pages/listings.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {Link} from "@inertiajs/inertia-svelte";
+
+    export let listings
+    export let heading
+</script>
+
+
+<h1>{heading}</h1>
+
+<div>
+    {#if listings.length === 0}
+        <p>No listings found</p>
+    {/if}
+    {#each listings as {id, title, description}}
+        <Link href="/listing/{id}">
+            <h2>{title}</h2>
+        </Link>
+        <p>{description}</p>
+    {/each}
+</div>
