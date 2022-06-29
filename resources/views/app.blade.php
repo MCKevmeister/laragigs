5896b63b1f4763760bdc10d6fed29703bc3c0f8a
MCKevmeister <markchristison@gmail.com>
2022-06-29 12:00:13 +1200

    add stylesheet link to blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
     @inertiaHead
+    <title>LaraGigs | Find Laravel Jobs & Projects</title>
 </head>
 <body>
 @inertia
