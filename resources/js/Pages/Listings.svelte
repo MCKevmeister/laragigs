e3e13d0474a5068d653ca4219aa72cdb17cb5359
MCKevmeister <markchristison@gmail.com>
2022-06-29 17:00:17 +1200

    fix style logic

--- resources/js/Pages/Listings.svelte
+++ resources/js/Pages/Listings.svelte
@@ -15,13 +15,11 @@
 <Hero />
 <Search />
 
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-    <div>
-        {#if listings.length === 0}
-            <p>No listings found</p>
-        {/if}
-        {#each listings as listing}
-           <ListingItem {... listing}/>
-        {/each}
-    </div>
+<div class="lg:grid lg:grid-cols-2 gap-2 space-y-4 md:space-y-0 mx-4">
+    {#if listings.length === 0}
+        <p>No listings found</p>
+    {/if}
+    {#each listings as listing}
+        <ListingItem {...listing}/>
+    {/each}
 </div>
