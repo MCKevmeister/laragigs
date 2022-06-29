3e9bb1028a73a19f88247b368b313bcec76e26ef
MCKevmeister <markchristison@gmail.com>
2022-06-29 15:46:37 +1200

    rename and add listing page and ListingItem

---
+++ resources/js/Pages/Components/ListingItem.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { inertia } from "@inertiajs/inertia-svelte";
+
+    export let title, company, description, location, type, url, id;
+</script>
+
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+        <img class="hidden w-48 mr-6 md:block"
+             src="images/no-image.png"
+             alt=""/>
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{id}" use:inertia>{title}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{company}</div>
+            <ul class="flex">
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                    <a href="#">Laravel</a>
+                </li>
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                    <a href="#">API</a>
+                </li>
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                    <a href="#">Backend</a>
+                </li>
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                    <a href="#">Vue</a>
+                </li>
+            </ul>
+            <div class="text-lg mt-4">
+                {location}
+            </div>
+        </div>
+    </div>
+</div>
--- resources/js/Pages/Listing.svelte
+++ resources/js/Pages/Listing.svelte
@@ -1,36 +1,18 @@
-<script>
-    //import {Link} from "@inertiajs/inertia-svelte";
+<script context="module">
+    import Layout from './Layout.svelte'
 
-    export let title, company, description, location, type, url, id;
+    export const layout = Layout
 </script>
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
-    <div class="flex">
-        <img class="hidden w-48 mr-6 md:block"
-             src="images/acme.png"
-             alt=""/>
-        <div>
-            <h3 class="text-2xl">
-                <a href="show.html">{title}</a>  <!-- make a inertia link -->
-            </h3>
-            <div class="text-xl font-bold mb-4">{company}</div>
-            <ul class="flex">
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Laravel</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">API</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Backend</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
-            <div class="text-lg mt-4">
-                {location}
-            </div>
-        </div>
-    </div>
-</div>
+<script>
+
+import Search from "./Components/Search.svelte";
+
+export let title;
+export let description;
+</script>
+
+<Search />
+
+<h2>{title}</h2>
+<p>{description}</p>
